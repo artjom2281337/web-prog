19debdd655b56bf89e4010febd028434f37c6a5f
Artjom Smorgulenko <artjomsmorgulenko@gmail.com>
2025-02-13 17:21:13 +0200

    Update index.php

--- webprogramming25/src/index.php
+++ webprogramming25/src/index.php
@@ -1,4 +1,30 @@
 <?php
-echo "<h1>hello world this is test</h1>";
-// phpinfo();
+require 'user/db.php';
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post">
+        <input type="text" name="search" class="search"><input type="submit" name="submit" value="Search for data">
+    </form>
+
+    <?php 
+        $search = $_POST['search'];
+        $sql = "SELECT * FROM 'search' WHERE 'title' LIKE '%$search%' OR 'text' LIKE '%$search%'";
+        $result = $conn->query($sql);
+        if($result->num_rows > 0) {
+            echo "<h1>". $row["title"] ."</h1><p>". $row["text"]."</p><br>";
+        } while($row = $result->fetch_assoc()) {
+        }
+
+
+    
+    ?>
+</body>
+</html>
